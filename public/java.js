75a70c61b6267b392bcd4b0acec9127355512796
driedclover <moistenclover@gmail.com>
2026-03-04 16:33:06 -0500

    .

---
+++ public/java.js
@@ -0,0 +1,27 @@
+// 1) Get the game container from HTML
+const gameContainer = document.querySelector(".game");
+
+// 2) Define board size
+const rows = 6;
+const columns = 5;
+
+/********************
+ Build board
+********************/
+
+function createBoard() {
+  // Create rows * columns tiles (6 * 5 = 30)
+  for (let i = 0; i < rows * columns; i++) {
+    const box = document.createElement("div");
+    box.className = "box";
+    gameContainer.appendChild(box);
+  }
+  
+}
+
+createBoard();
+
+  for (intiliazer; condition; update)
+
+  or 
+  for (let i = 0; i < rows * columns; i++)
