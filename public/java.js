45f29ad41a1474842ae53537b6a26803468ba165
driedclover <moistenclover@gmail.com>
2026-03-04 16:49:18 -0500

    ...

--- public/java.js
+++ public/java.js
@@ -21,7 +21,53 @@ function createBoard() {
 
 createBoard();
 
-  for (intiliazer; condition; update)
 
-  or 
-  for (let i = 0; i < rows * columns; i++)
+let currentBox = 0;
+let currentRow = 1;
+let currentGuess = [];
+
+function rowStart(row) {
+  return columns * (row - 1);
+}
+
+function rowEnd(row) {
+  return columns * row;
+}
+
+/********************
+Input loop (event listener)
+********************/
+// keydown event, e = event object, 
+document.addEventListener("keydown", (e) => {
+  // e.key = key that was pressed
+  const key = e.key;
+
+  // Stop typing if the row is full
+  if (currentBox >= rowEnd(currentRow)) return;
+
+  // Only accept letters A-Z ( uppercase or lowsercase)
+  if (key.length === 1 && key.match(/[a-z]/i)) {
+    boxes[currentBox].innerHTML = `<span class="letter">${ key.toUpperCase() }</span>`;
+    currentGuess = [...currentGuess, key.toUpperCase()];
+    currentBox++;
+  }
+});
+
+document.addEventListener("keydown"), (e) => {
+  const key = e.key;
+
+  //////////////////////
+  // PREVIOUS CODE ⬆️
+  //////////////////////
+
+  // BACKSPACE: delete the previous letter
+  if (key === "Backspace") {
+    // Don’t let them delete into the previous row
+    if (currentBox > rowStart(currentRow)) {
+      currentBox--;
+      boxes[currentBox].textContent = "";
+      currentGuess.pop();
+    }
+    return;
+  }
+}
