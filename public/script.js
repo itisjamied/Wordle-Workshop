11acb4e35cb66889b0184a9b441f2d1d2525c540
driedclover <moistenclover@gmail.com>
2026-03-04 15:42:40 -0500

    .

--- public/script.js
+++ public/script.js
@@ -38,3 +38,60 @@ function rowStart(row) {
     return columns * (row - 1);
 }
 
+function rowEnd(row) {
+    return columns * row;
+}
+
+
+/********************
+Input loop (event listener)
+********************/
+// keydown event, e = event object, 
+document.addEventListener("keydown", (e) => {
+  // e.key = key that was pressed
+  const key = e.key;
+
+  // Stop typing if the row is full
+  if (currentBox >= rowEnd(currentRow)) return;
+
+  // Only accept letters A-Z ( uppercase or lowsercase)
+  if (key.length === 1 && key.match(/[a-z]/i)) {
+    boxes[currentBox].innerHTML = `<span class="letter">${ key.toUpperCase() }</span>`;
+    currentGuess = [...currentGuess, key.toUpperCase()];
+    currentBox++;
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+
+  //////////////////////
+  // PREVIOUS CODE ⬆️
+  //////////////////////
+
+  // BACKSPACE: delete the previous letter
+  if (key === "Backspace") {
+    // Don’t let them delete into the previous row
+    if (currentBox > rowStart(currentRow)) {
+      currentBox--;
+      boxes[currentBox].textContent = "";
+      currentGuess.pop();
+    }
+    return;
+  }
+
+  //////////////////////
+  // PREVIOUS CODE ⬇️
+  //////////////////////
+
+
+  // Stop typing if the row is full
+  if (currentBox >= rowEnd(currentRow)) return;
+
+   // Only accept letters A-Z ( uppercase or lowsercase)
+  if (key.length === 1 && key.match(/[a-z]/i)) {
+    boxes[currentBox].innerHTML = `<span class="letter">${ key.toUpperCase() }</span>`;
+    currentGuess.push(key.toUpperCase());
+    currentBox++;
+  }
+});
