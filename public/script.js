ae04512d66e29bfc2acdf09660b35a989a10cba4
MrO3420 <omason632@gmail.com>
2026-03-18 17:57:27 -0400

    Wordle almost done.

--- public/script.js
+++ public/script.js
@@ -13,3 +13,37 @@ createBoard(); const boxes = document.querySelectorAll(".box");
 let currentBox = 0;
 let currentRow = 1;
 let currentGuess = [];
+
+function rowStart(row) {
+  return columns * (row - 1);
+}
+
+function rowEnd(row) {
+  return columns * row;
+}
+
+document.addEventListener("keydown", (e) => {
+const key = e.key;
+if (currentBox >= rowEnd(currentRow)) return;
+if (key.length === 1 && key.match(/[a-z]/i)) {
+    boxes[currentBox].innerHTML = `<span class="letter">${ key.toUpperCase() }</span>`;
+    currentGuess = [...currentGuess, key.toUpperCase()];
+    currentBox++;
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  const key = e.key;
+   if (key === "Backspace") { if (currentBox > rowStart(currentRow)) {
+      currentBox--;
+      boxes[currentBox].textContent = "";
+      currentGuess.pop();
+    }
+    return;
+  }
+  if (currentBox >= rowEnd(currentRow)) return;if (key.length === 1 && key.match(/[a-z]/i)) {
+    boxes[currentBox].innerHTML = `<span class="letter">${ key.toUpperCase() }</span>`;
+    currentGuess = [...currentGuess, key.toUpperCase()];
+    currentBox++;
+  }
+});
